5d20e85c24c3606a73256834efe5ab905622a2a0
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2024-11-07 21:20:25 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
@@ -9,7 +11,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,30 @@
-import { Container, Col, Row } from 'reactstrap';
-
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <footer className='site-footer'>
             <Container>
                 <Row>
-                    <Col xs={{ size: 4, offset: 1 }} sm='2'><h5>this will be site navigation!</h5></Col>
-                    <Col xs='6' sm='3' className='text-center'><h5>Social</h5>
+                    <Col xs={{ size: 4, offset: 1 }} sm='2'>
+                        <h5>Links</h5>
+                        <ul className='list-unstyled'>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/directory'>Directory</Link>
+                            </li>
+                            <li>
+                                <Link to='/about'>About</Link>
+                            </li>
+                            <li>
+                                <Link to='/contact'>Contact</Link>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col xs='6' sm='3' className='text-center'>
+                        <h5>Social</h5>
                         <a
                             className='btn btn-social-icon btn-instagram'
                             href='http://instagram.com/'
@@ -31,8 +48,10 @@ const Footer = () => {
                             href='http://youtube.com/'
                         >
                             <i className='fa fa-youtube' />
-                        </a></Col>
-                    <Col sm='4' className='text-center'><a
+                        </a>
+                    </Col>
+                    <Col sm='4' className='text-center'>
+                        <a
                             role='button'
                             className='btn btn-link'
                             href='tel:+12065551234'
@@ -46,11 +65,12 @@ const Footer = () => {
                             href='mailto:notreal@notreal.co'
                         >
                             <i className='fa fa-envelope-o' /> campsites@nucamp.co
-                        </a></Col>
+                        </a>
+                    </Col>
                 </Row>
             </Container>
         </footer>
-    )
-}
+    );
+};
 
 export default Footer;
