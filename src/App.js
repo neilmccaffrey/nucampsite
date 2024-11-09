b8e5ade34690d2036067d70c9356da760ebd286e
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2024-11-09 12:18:03 -0500

    Partners

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import AboutPage from './pages/AboutPage';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
         <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+        <Route path='about' element={<AboutPage />}/>
       </Routes>
       <Footer />
     </div>
