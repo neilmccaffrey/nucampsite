31e5140fe875efc60168a50e79e86af41217e569
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2024-11-27 16:40:32 -0500

    loading/error

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -5,18 +5,34 @@ import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CommentsList from '../features/comments/CommentsList';
 import SubHeader from '../components/SubHeader';
 import { useSelector } from 'react-redux';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
 
 const CampsiteDetailPage = () => {
     const { campsiteId } = useParams();
     const campsite = useSelector(selectCampsiteById(campsiteId));
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
+    let content = null;
+
+
+    if (isLoading) {
+        content = <Loading />;
+    } else if (errMsg) {
+        content = <Error errMsg={errMsg} />;
+    } else {
+        content = (
+            <>
+                <CampsiteDetail campsite={campsite} />
+                <CommentsList campsiteId={campsiteId} />
+            </>
+        );
+    }
 
     return (
         <Container>
-            <SubHeader current={campsite.name} detail={true} />
-            <Row>
-                <CampsiteDetail campsite={campsite} />
-                <CommentsList campsiteId={campsiteId} />
-            </Row>
+            {campsite && <SubHeader current={campsite.name} detail={true} />}
+            <Row>{content}</Row>
         </Container>
     );
 };
